fbcfb964305c138540080bd07c01e0796ac79d2f
Eduardomesut <eduardomesut@gmail.com>
2025-06-05 11:58:23 +0200

    Base de api-rest

---
+++ ariabankback/src/main/java/com/ariabank/ariabankback/AriabankbackApplication.java
@@ -0,0 +1,13 @@
+package com.ariabank.ariabankback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AriabankbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AriabankbackApplication.class, args);
+	}
+
+}
